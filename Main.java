b3913b811170f73f017c9eea81819af616ef2ead
filipbezserca <f.komisarek@gmail.com>
2020-08-05 08:26:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+class Main {
+  public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("plik.txt");
+        Scanner read = new Scanner(file);
+        System.out.print("wczytuje linie");
+        String text = read.nextLine();
+        //System.out.print(text);
+
+        PrintWriter write = new PrintWriter("plik.txt");
+        write.println("tekst");
+        write.close();
+
+        Scanner in = new Scanner(System.in);
+    
+        System.out.print("Jak sie nazywasz? " + text);
+        String name = in.nextLine();
+    
+        System.out.print("Ile masz lat? ");
+        int age = in.nextInt();
+    
+        System.out.printf("Witaj, %s. W przyszlym roku bedziesz miec %d lat.", name, age);
+    }
+}
